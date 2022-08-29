bd52384656c3b695b916810e8cc93998939d071e
JoonLind <joonas.lindell@outlook.com>
2022-08-29 12:35:57 +0300

    End of section seven

---
+++ client-app/src/features/activities/details/ActivityDetails.tsx
@@ -0,0 +1,34 @@
+import React from 'react';
+import { Button, Card, Icon, Image } from 'semantic-ui-react';
+import { Activity } from '../../../app/modules/activity';
+
+
+interface Props {
+    activity: Activity;
+    cancelSelectActivity: () => void; 
+    openForm: (id: string) => void;
+}
+
+export default function ActivityDetails({activity, cancelSelectActivity, openForm}: Props) {
+    return (
+      //Kuva ei toimi, ei tee Javascriptiä jostain syystä
+<Card >   
+    <Image src={`/assets/categoryImages/${activity.category}.jpg `}/>         
+    <Card.Content>
+      <Card.Header>{activity.title}</Card.Header>
+      <Card.Meta>
+        <span>{activity.date}</span>
+      </Card.Meta>
+      <Card.Description>
+        {activity.description}
+      </Card.Description>
+    </Card.Content>
+    <Card.Content extra>
+      <Button.Group widths='2'>
+        <Button onClick={() => openForm(activity.id)} basic color='blue' content='Edit' />
+        <Button onClick={cancelSelectActivity} color='grey' content='Cancel' />
+      </Button.Group>
+    </Card.Content>
+  </Card>
+    )
+}
