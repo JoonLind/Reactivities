419604fa1bc46c4a235bbc6aa188cde90682460c
JoonLind <joonas.lindell@outlook.com>
2022-10-31 09:41:33 +0200

    End of 13

--- API/Controllers/AccountController.cs
+++ API/Controllers/AccountController.cs
@@ -49,11 +49,13 @@ namespace API.Controllers
         {
             if (await _userManager.Users.AnyAsync(x => x.Email == registerDto.Email))
             {
-                return BadRequest("Email taken");
+                ModelState.AddModelError("email", "Email taken");
+                return ValidationProblem();
             }
-             if (await _userManager.Users.AnyAsync(x => x.Email == registerDto.UserName))
+             if (await _userManager.Users.AnyAsync(x => x.UserName == registerDto.UserName))
             {
-                return BadRequest("Username taken");
+                ModelState.AddModelError("username", "Username taken");
+                return ValidationProblem();
             }
 
             var user = new AppUser{
@@ -70,7 +72,7 @@ namespace API.Controllers
 
             }
 
-            return BadRequest("Problem registerin user");
+            return BadRequest("Problem registering user");
         }
         [Authorize]
         [HttpGet]
--- client-app/src/app/layout/App.tsx
+++ client-app/src/app/layout/App.tsx
@@ -10,15 +10,33 @@ import TestErrors from '../../features/errors/TestError';
 import { ToastContainer } from 'react-toastify';
 import NotFound from '../../features/errors/NotFound';
 import ServerError from '../../features/errors/ServerError';
+import LoginFomr from '../../features/users/LoginForm';
+import LoginForm from '../../features/users/LoginForm';
+import { useStore } from '../stores/store';
+import LoadingComponent from './LoadingComponent';
+import { useEffect } from 'react';
+import ModalContainer from '../common/modals/ModalContainer';
 
 
 
 function App() {
   const location = useLocation();
+  const {commonStore, userStore} = useStore();
+
+  useEffect(() => {
+    if (commonStore.token) {
+      userStore.getUser().finally(() => commonStore.setAppLoaded());
+    } else {
+      commonStore.setAppLoaded();
+    }
+  }, [commonStore, userStore])
+
+  if (!commonStore.appLoaded) return <LoadingComponent content='Loading app...' />
 
   return (
     <>
     <ToastContainer position='bottom-right' hideProgressBar />
+    <ModalContainer />
       <Route exact path='/' component={HomePage} />
       <Route
         path={'/(.+)'}
@@ -31,7 +49,8 @@ function App() {
                 <Route path='/activities/:id' component={ActivityDetails} />
                 <Route key={location.key} path={['/createActivity', '/manage/:id']} component={ActivityForm} />
                 <Route path='/errors' component={TestErrors} />
-                <Route path='/servererror' component={ServerError} />
+                <Route path='/server-error' component={ServerError} />
+                <Route path='/login' component={LoginForm} />
                 <Route component={NotFound} />
               </Switch>
             </Container>
